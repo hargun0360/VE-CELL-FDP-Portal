e568773d17c62c002453dd7806bacf2d75728234
hargun0360 <hargunsingh0720@gmail.com>
2022-08-23 10:40:40 +0530

    Complete College Form

--- src/College/CollegeForm.js
+++ src/College/CollegeForm.js
@@ -28,6 +28,7 @@ function CollegeForm() {
       setFlag2(true)
     }
   }, [flag1, ftype, flag2])
+  const today = new Date();
   const handleSubmit = (e) => {
     e.preventDefault();
     if (name && department && email && mobile && designation && ftype && (online || offline) && incentive) {
@@ -52,6 +53,11 @@ function CollegeForm() {
       <Card className='w-75 h-100 mt-3'>
         <Card.Body className='w-100'>
           <Form onSubmit={(e) => handleSubmit(e)}>
+            <Row className='mb-3 mt-3'>
+              <Card.Title>
+                Faculty Detail
+              </Card.Title>
+            </Row>
             <Row>
               <Col xs={12} md={6}>
                 <Form.Group className="mb-3" controlId="formBasicName">
@@ -97,6 +103,13 @@ function CollegeForm() {
                   <Form.Control type="text" placeholder="HOD,Dean,etc." required onChange={(e) => setDesignation(e.target.value)} />
                 </Form.Group>
               </Col>
+            </Row>
+            <Row className='mb-3 mt-3'>
+              <Card.Title>
+                FDP type
+              </Card.Title>
+            </Row>
+            <Row>
               <Col xs={12} md={6}>
                 <Form.Group className="mb-3">
                   <Form.Label>FDP type</Form.Label>
@@ -143,22 +156,76 @@ function CollegeForm() {
                 </Form.Group>
               </Col>
             </Row>
-            <Form.Group className="mb-3">
-              <Form.Label>Incentive Details</Form.Label>
-              <Form.Select className='mb-3' aria-label="Default select example" required onChange={(e) => setIncentive(e.target.value)}>
-                <option>Select the Incentive Details</option>
-                <option value="AKTU Level-2 (10,000)">AKTU Level-2 (10,000)</option>
-                <option value="AKTU Level-3 (15,000)">AKTU Level-3 (15,000)</option>
-                <option value="AICTE UHV-III (10,000)">AICTE UHV-III (10,000)</option>
-                <option value="AICTE UHV-IV (15,000)">AICTE UHV-IV (15,000)</option>
-                <option value="Not Taken Yet">Not Taken Yet</option>
-              </Form.Select>
-            </Form.Group>
+            <Row className='mb-3 mt-3'>
+              <Card.Title>
+                FDP Detail
+              </Card.Title>
+            </Row>
+            <Row>
+              <Col xs={12} md={3}>
+                <Form.Group className="mb-3" controlId="exampleForm.ControlInput2">
+                  <Form.Label>Starting Date</Form.Label>
+                  <Form.Control type="date" placeholder="Starting Date" min={new Date().toISOString().split('T')[0]} required onChange={(e) => setEmail(e.target.value)} />
+                </Form.Group>
+              </Col>
+              <Col xs={12} md={3}>
+                <Form.Group className="mb-3" controlId="exampleForm.ControlInput3">
+                  <Form.Label>End Date</Form.Label>
+                  <Form.Control type="date" placeholder="End Date" min={new Date().toISOString().split('T')[0]} required onChange={(e) => setEmail(e.target.value)} />
+                </Form.Group>
+              </Col>
+              <Col xs={12} md={2}>
+                <Form.Group className="mb-3" controlId="exampleForm.ControlInput3">
+                  <Form.Label>Number of Days</Form.Label>
+                  <Form.Control type="number" placeholder="No. of Days" required onChange={(e) => setEmail(e.target.value)} />
+                </Form.Group>
+              </Col>
+              <Col xs={12} md={4}>
+                <Form.Group className="mb-3" controlId="exampleForm.ControlInput3">
+                  <Form.Label>Venue</Form.Label>
+                  <Form.Control type="text" placeholder="Venue" required onChange={(e) => setEmail(e.target.value)} />
+                </Form.Group>
+              </Col>
+            </Row>
+            <Row className='mb-4'>
+              <Col xs={12} md={6}>
+                <Form.Group className="mb-3" controlId="exampleForm.ControlInput3">
+                  <Form.Label>AICTE/AKTU Certificate Number</Form.Label>
+                  <Form.Control type="text" placeholder="Certificate Number" required onChange={(e) => setEmail(e.target.value)} />
+                </Form.Group>
+              </Col>
+              <Col xs={12} md={4}>
+                <Form.Group className="mb-3" controlId="exampleForm.ControlInput3">
+                  <Form.Label >Upload Certificate copy</Form.Label>
+                  <Form.Control type="file" required onChange={(e) => setEmail(e.target.value)} />
+                </Form.Group>
+              </Col>
+            </Row>
+            <Row className='mb-3 mt-3'>
+              <Card.Title>
+                Incentive Details
+              </Card.Title>
+            </Row>
+            <Row className="mb-3">
+              <Col xs={12} md={6}>
+              <Form.Group className="mb-3">
+                <Form.Label>Incentive Details</Form.Label>
+                <Form.Select className='mb-3' aria-label="Default select example" required onChange={(e) => setIncentive(e.target.value)}>
+                  <option>Select the Incentive Details</option>
+                  <option value="AKTU Level-2 (10,000)">AKTU Level-2 (10,000)</option>
+                  <option value="AKTU Level-3 (15,000)">AKTU Level-3 (15,000)</option>
+                  <option value="AICTE UHV-III (10,000)">AICTE UHV-III (10,000)</option>
+                  <option value="AICTE UHV-IV (15,000)">AICTE UHV-IV (15,000)</option>
+                  <option value="Not Taken Yet">Not Taken Yet</option>
+                </Form.Select>
+              </Form.Group>
+              </Col>
+            </Row>
             <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1" onChange={(e) => setRemarks(e.target.value)}>
               <Form.Label>Remarks</Form.Label>
               <Form.Control as="textarea" rows={2} />
             </Form.Group>
-            <Button variant="primary" style={{float:"right"}} type="submit" className='w-sm-100'>
+            <Button variant="primary" style={{ float: "right" }} type="submit" className='w-sm-100'>
               Submit
             </Button>
           </Form>
