d788e5aafba8af556658491a0b68504733a1b080
hargun0360 <hargunsingh0720@gmail.com>
2022-09-20 04:45:52 +0530

    Adding Private Routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import  CollegeForm  from './College/CollegeForm';
+import CollegeForm from './College/CollegeForm';
 import Login from './Authentication/Login';
 import Signup from './Authentication/Sign-Up';
 import OTP from './Authentication/OTP';
@@ -9,22 +9,43 @@ import RecoverPass from './Authentication/RecoverPass';
 import ViewDetail from './College/ViewDetail';
 import ViewFDP from './College/ViewFDP';
 import { Routes, Route } from 'react-router-dom'
+import PrivateRoute from './Routes/PrivateRoutes';
 function App() {
   return (
     <div className="App">
-       <Routes>
-       <Route exact path="/" element={<Login />} />
-       <Route exact path="/view" element={<ViewDetail />} />
-       <Route exact path="/view/:id" element={<ViewDetail />} />
-       <Route exact path="/form" element={<CollegeForm />} />
-       <Route exact path="/form/:id" element={<CollegeForm />} />
-       <Route exact path="/signup" element={<Signup />} />
-       <Route exact path="/forgot" element={<Forgot />} />
-       <Route exact path="/otp" element={<OTP />} />
-       <Route exact path="/reset" element={<Reset />} />
-       <Route exact path="/recoverpass" element={<RecoverPass />} />
-       <Route exact path="/viewall" element={<ViewFDP />} />
-       </Routes>
+      <Routes>
+        <Route exact path="/" element={<Login />} />
+        <Route exact path="/view" element={
+          <PrivateRoute>
+            <ViewDetail />
+          </PrivateRoute>} />
+        <Route exact path="/view/:id" element={<PrivateRoute>
+          <ViewDetail />
+        </PrivateRoute>} />
+        <Route exact path="/form" element={
+          <PrivateRoute>
+            <CollegeForm />
+          </PrivateRoute>} />
+        <Route exact path="/form/:id" element={
+          <PrivateRoute>
+            <CollegeForm />
+          </PrivateRoute>} />
+        <Route exact path="/signup" element={<Signup />} />
+        <Route exact path="/forgot" element={<Forgot />} />
+        <Route exact path="/otp" element={
+          <PrivateRoute>
+            <OTP />
+          </PrivateRoute>} />
+        <Route exact path="/reset" element={
+          <PrivateRoute>
+            <Reset />
+          </PrivateRoute>} />
+        {/* <Route exact path="/recoverpass" element={<RecoverPass />} /> */}
+        <Route exact path="/viewall" element={
+          <PrivateRoute>
+            <ViewFDP />
+          </PrivateRoute>} />
+      </Routes>
     </div>
   );
 }
