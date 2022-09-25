9bc1c2a7321236b63cec473da86284b5b63f54b1
hargun0360 <hargunsingh0720@gmail.com>
2022-09-25 20:51:52 +0530

    UI bug fixed

--- src/Authentication/OTP.js
+++ src/Authentication/OTP.js
@@ -29,82 +29,84 @@ const OTP = () => {
 
     const handleClick = () => {
         setMinutes(1);
-        if (location.state.otp == "forgot"){
-            
-        }else{
+        if (location.state.otp == "forgot") {
+
+        } else {
             doSignupUser({ email: location.state.email, })
-            .then((res) => {
-                console.log(res);
-                swal({
-                    title: "OTP will sent again",
-                    text: "",
-                    icon: "success",
-                    button: "OK",
-                  });
-            }).catch((e) => {
-                console.log(e);
-                swal({
-                    title: e.data.status,
-                    text: "",
-                    icon: "error",
-                    button: "OK",
-                  });
-            })
+                .then((res) => {
+                    console.log(res);
+                    swal({
+                        title: "OTP will sent again",
+                        text: "",
+                        icon: "success",
+                        button: "OK",
+                    });
+                }).catch((e) => {
+                    console.log(e);
+                    swal({
+                        title: e.data.status,
+                        text: "",
+                        icon: "error",
+                        button: "OK",
+                    });
+                })
         }
     }
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        setLoading(true);
         console.log(location.state.otp);
-        let obj = {
-            email: location.state.email,
-            otp: otp
-        }
-        console.log(obj);
-        if (location.state.otp == "forgot") {
-            doVerifyResetOtp(obj)
-                .then((res) => {
-                    console.log(res);
-                    setLoading(false);
-                    swal({
-                        title: "OTP is Verified",
-                        text: null,
-                        icon: "success",
-                        button: "OK",
-                      });
-                    navigate("/recoverpass");
-                }).catch((e) => {
-                    console.log(e);
-                    setLoading(false);
-                    swal({
-                        title: e.data.status,
-                        text: "",
-                        icon: "error",
-                        button: "OK",
-                      });
-                })
-        } else {
-            doVerifyUser(obj)
-                .then((res) => {
-                    console.log(res);
-                    swal({
-                        title: "Password will sent in Your Email",
-                        text: "",
-                        icon: "success",
-                        button: "OK",
-                      });
-                    navigate("/");
-                }).catch((e) => {
-                    console.log(e);
-                    setLoading(false);
-                    swal({
-                        title: e.data.status,
-                        text: "",
-                        icon: "error",
-                        button: "OK",
-                      });
-                })
+        if (otp) {
+            setLoading(true);
+            let obj = {
+                email: location.state.email,
+                otp: otp
+            }
+            console.log(obj);
+            if (location.state.otp == "forgot") {
+                doVerifyResetOtp(obj)
+                    .then((res) => {
+                        console.log(res);
+                        setLoading(false);
+                        swal({
+                            title: "OTP is Verified",
+                            text: "",
+                            icon: "success",
+                            button: "OK",
+                        });
+                        navigate("/recoverpass");
+                    }).catch((e) => {
+                        console.log(e);
+                        setLoading(false);
+                        swal({
+                            title: e.data.status,
+                            text: "",
+                            icon: "error",
+                            button: "OK",
+                        });
+                    })
+            } else {
+                doVerifyUser(obj)
+                    .then((res) => {
+                        console.log(res);
+                        swal({
+                            title: "Password will sent in Your Email",
+                            text: "",
+                            icon: "success",
+                            button: "OK",
+                        });
+                        navigate("/");
+                    }).catch((e) => {
+                        console.log(e);
+                        setLoading(false);
+                        swal({
+                            title: e.data.status,
+                            text: "",
+                            icon: "error",
+                            button: "OK",
+                        });
+                    })
+            }
         }
     }
 
@@ -161,7 +163,7 @@ const OTP = () => {
                                                 <p className="mb-5">
                                                     Please enter the code sent to{" "}
                                                     <span className="fw-bolder">
-                                                        example@akgec.ac.in
+                                                        {location.state.email}
                                                     </span>
                                                 </p>
 
@@ -172,10 +174,10 @@ const OTP = () => {
                                                                 <AuthCode
                                                                     characters={4}
                                                                     className="form-control form-control-lg text-center"
-                                                                    allowedCharacters="^[0-9]"
+                                                                    allowedCharacters="[0-9]"
                                                                     inputStyle={{
-                                                                        width: "76px",
-                                                                        height: "42px",
+                                                                        width: "50px",
+                                                                        height: "45px",
                                                                         padding: "8px",
                                                                         borderRadius: "8px",
                                                                         fontSize: "16px",
@@ -189,9 +191,11 @@ const OTP = () => {
                                                             </FormGroup>
                                                         </Col>
                                                     </Row>
-                                                    <button className="btn btn-primary w-100 mt-3 d-grid" type='submit' >
-                                                        Confirm
-                                                    </button>
+                                                    <div style={{ display: "flex", justifyContent: "center" }}>
+                                                        <button className="btn btn-primary w-75 mt-3 d-grid" type='submit' >
+                                                            Confirm
+                                                        </button>
+                                                    </div>
                                                 </Form>
                                             </div>
                                         </div>
